a8b79efcce82da6cbe5f5d7d938424566e6ba6ff
Eren Güven <erenguven0@gmail.com>
2016-10-27 22:35:11 +0200

    add Command and execution of player commands

--- mobai/engine/game.py
+++ mobai/engine/game.py
@@ -6,22 +6,60 @@ from .tile import GameTile
 
 
 class ActionType(enum.Enum):
-    clear = 0
-    target = 1
+    target = 0
+    clear_target = 1
+    stop = 2
 
 
-class Action(object):
-    def __init__(self, player, unit, _type, target=None):
-        assert player == unit.player
-        assert _type in ActionType.__members__
+class Command(object):
+    '''a command received from a player
+    {'id': '<uuid>', 'action': '<action-type>.name', 'target': '<uuid>' | {'posx': X, 'posy': Y} }
+    '''
+    def __init__(self, player, command):
+        # id present and not empty
+        assert command.get('id') and isinstance(command['id'], str)
+        # action present and valid
+        assert 'action' in command and command['action'] in ActionType.__members__
+        if ActionType[command['action']] is ActionType.target:
+            # target present and valid
+            assert 'target' in command and command['target']
+            assert isinstance(command['target'], (str, dict))
+        self.id = command['id']
+        self.action = ActionType[command['action']]
+        if self.action is ActionType.target:
+            self.target = command['target']
+            if isinstance(self.target, dict):
+                # position target valid
+                assert 'posx' in self.target and 'posy' in self.target
+                assert isinstance(self.target['posx'], int)
+                assert isinstance(self.target['posy'], int)
         self.player = player
-        self.unit = unit
-        self.action_type = ActionType[_type]
+
+    def verify_unit(self, units):
+        '''check if id is correct and player owns unit'''
+        assert self.id in units and units[self.id].player == self.player
+        self.unit = units[self.id]
+
+    def verify_target(self, units, map):
+        '''make sure target is valid'''
+        if isinstance(command.target, str):  # targeting a unit
+            assert command.target in units and units[command.target].player != self.player:
+            target = units[command.target]
+        elif isinstance(command.target, dict):  # targeting a tile (position)
+            assert self.unit.mobile
+            target = map.get_tile(command.target['posx'], command.target['posy'])
+        assert map.player_has_vision(target)
         self.target = target
-        if self.action_type is ActionType.target:
-            assert target is not None
-            if isinstance(target, GameTile):
-                assert unit.mobile
+
+    def execute(self):
+        if self.action is ActionType.target:
+            self.set_target(self.target)
+        elif self.action is ActionType.clear_target:
+            self.clear_target()
+        elif self.action is ActionType.stop:
+            self.stop()
+        else:
+            raise Exception('Uhm?')
 
 
 class GameState(object):
@@ -54,8 +92,26 @@ class GameState(object):
             map=self.map.to_array(by_player=player),
         )
 
-    def orders_from_player(self, orders):
-        raise NotImplementedError
+    def commands_from_player(self, player, commands):
+        '''actions are limited to total unit count, extras will be trimmed
+        from the beginning
+        '''
+        unit_lookup = {unit.id: unit for unit in self.map.get_all_units()}
+        actions = []
+        errors = []  # TODO maybe feedback, maybe clear error definitions
+        commands = commands[-1 * len(units):]
+        for command in commands:
+            try:
+                cmd = Command(player, command)
+                cmd.verify_unit(unit_lookup)
+                if cmd.action is ActionType.target:
+                    cmd.verify_target(unit_lookup, self.map)
+            except AssertionError:
+                errors.append(command)
+                continue
+            cmd.execute()
+            actions.append(command)
+        return dict(actions=actions, errors=errors)
 
     def _spawn_new_units(self):
         for fort in self.map.get_forts():
