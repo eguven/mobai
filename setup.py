731d44ea8568bd0135cad5c61cd650cb353491d1
eguven <erenguven0@gmail.com>
2026-02-17 22:52:14 +0100

    chore: update setup.py and start using uv

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='mobai',
-    version='0.1',
+    version='0.1.0.dev0',
     description='A very simple moba-inspired game engine for bots',
     long_description=open('README.md', 'r').read(),
     url='https://github.com/eguven/mobai',
